12b3a7dcd13315df17f5a7efa52326e2741f1813
Mahmud Oduoye <mooduoye@gmail.com>
2021-03-11 21:34:06 +0100

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using ASP.NET_Login_Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_Login_Form.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("LoginForm");
+        }
+
+        public ActionResult Login(UserModel userModel)
+        {
+            if (userModel.Username =="admin" && userModel.Password == "superuser")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                ViewBag.Message = "Login Failled!.";
+                return View("LoginForm");
+            }
+            
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Login_Form.Models
+{
+    public class UserModel
+    {
+        // user attributes
+        [Required]
+        public String Username { get; set; }
+        [Required]
+        public String Password{ get; set; }
+
+
+        // construtor with default parameter
+        public UserModel()
+        {
+            Username = "Nothing";
+            Password = "Nothing";
+        }
+
+        // constructor with no default parameter
+        public UserModel(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        
+
+
+
+    }
+}
